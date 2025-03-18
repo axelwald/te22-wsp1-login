305f4d44560b344bfb6b97ff409d0679db937250
Axel Waldenborg <149167584+axelwald@users.noreply.github.com>
2025-03-18 09:30:21 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+import "dotenv/config"
+import express from "express"
+import nunjucks from "nunjucks"
+import logger from "morgan"
+import bcrypt from "bcrypt"
+import session from "express-session"
+
+const app = express()
+const port = 3000
+
+
+
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+})
+
+app.use(logger("dev"))
+app.use(express.static("public"))
+
+let myPlaintextPassword = "detlösenordsomduvillha"
+bcrypt.hash(myPlaintextPassword, 10, function(err, hash) {
+	// här får vi nu tag i lösenordets hash i variabeln hash
+	console.log(hash)
+
+
+})
+
+
+
+app.use(session({
+  secret: "keyboard cat ",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { sameSite: true }
+}))
+
+
+app.get("/", (req, res) => {
+  if (req.session.views) {
+    req.session.views++
+  } else {
+    req.session.views = 1
+  }
+  res.render("index.njk",
+    { title: "Test", message: "Funkar?", views: req.session.views }
+  )
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
